26d011deec8673cd91a19a88fdd414c509a04052
AIKAM BOPARAI <as2bopar@uwaterloo.ca>
2020-12-14 22:04:53 -0500

    Stack Min

---
+++ StackWithMin.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class StackWithMin{
+    Stack<Integer> mins;
+    Stack<Integer> values;
+
+    public StackWithMin(){
+        mins = new Stack<Integer>();
+        values = new Stack<Integer>();
+    }
+
+    public void push(int value){
+        if(value <= min()){
+            mins.push(value);
+        }
+        values.push(value);
+    }
+
+    public int pop(){
+        int value = values.pop();
+        if(value == min()){
+            mins.pop();
+        }
+        return value; 
+    }
+
+    public int min(){
+        if(mins.empty()){
+            return Integer.MAX_VALUE;
+        }
+        else{
+            return mins.peek();
+        }
+    }
+}
