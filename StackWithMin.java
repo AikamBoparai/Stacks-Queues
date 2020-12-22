b549870e5c1c9ae818099524d76b26de25003cdc
AIKAM BOPARAI <as2bopar@uwaterloo.ca>
2020-12-22 17:13:52 -0500

    Added test cases and class

--- StackWithMin.java
+++ StackWithMin.java
@@ -2,35 +2,58 @@ import java.util.Stack;
 
 public class StackWithMin{
     //We can use another stack to track the mins
-    Stack<Integer> mins;
-    Stack<Integer> values;
+    public static class StackWrapper{
+        Stack<Integer> mins;
+        Stack<Integer> values;
 
-    public StackWithMin(){
-        mins = new Stack<Integer>();
-        values = new Stack<Integer>();
+        public StackWrapper(){
+            mins = new Stack<Integer>();
+            values = new Stack<Integer>();
+        }
+
+        public void push(int value){
+            if(value <= min()){
+                mins.push(value);
+            }
+            values.push(value);
+        }
+
+        public int pop(){
+            int value = values.pop();
+            if(value == min()){
+                mins.pop();
+            }
+            return value; 
+        }
+
+        public int min(){
+            if(mins.empty()){
+                return Integer.MAX_VALUE;
+            }
+            else{
+                return mins.peek();
+            }
+        }
+
     }
 
-    public void push(int value){
-        if(value <= min()){
-            mins.push(value);
-        }
-        values.push(value);
+    public static void main(String[] args) {
+        StackWrapper values = new StackWrapper();
+        values.push(Integer.MAX_VALUE);
+        System.out.println(values.min());
+        values.push(Integer.MIN_VALUE);
+        System.out.println(values.min());
+        values.pop();
+        System.out.println(values.min());
+        values.push(5);
+        values.push(7);
+        values.push(3);
+        System.out.println(values.min());
+        values.pop();
+        values.pop();
+        System.out.println(values.min());
+        values.pop();
+        System.out.println(values.min());
     }
 
-    public int pop(){
-        int value = values.pop();
-        if(value == min()){
-            mins.pop();
-        }
-        return value; 
-    }
-
-    public int min(){
-        if(mins.empty()){
-            return Integer.MAX_VALUE;
-        }
-        else{
-            return mins.peek();
-        }
-    }
 }
