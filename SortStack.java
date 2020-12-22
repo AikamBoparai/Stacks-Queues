929b095dd49677533e0e1b57c41116240d80ad42
AIKAM BOPARAI <as2bopar@uwaterloo.ca>
2020-12-22 17:06:05 -0500

    Added tests

--- SortStack.java
+++ SortStack.java
@@ -1,3 +1,4 @@
+import java.util.Stack;
 class SortStack{
     public static void sort(Stack<Integer> s){
         Stack<Integer> buf = new Stack<Integer>();
@@ -15,5 +16,17 @@ class SortStack{
     public static void main(String[] args) {
         Stack<Integer> values = new Stack<Integer>();
         values.push(8);
+        values.push(8);
+        values.push(4);
+        values.push(Integer.MAX_VALUE);
+        values.push(Integer.MIN_VALUE);
+        values.push(99);
+        values.push(1);
+        values.push(7);
+        System.out.println(values.toString());
+        
+        //Sort so smallest elements are on top
+        sort(values);
+        System.out.println(values.toString());
     }
 }
